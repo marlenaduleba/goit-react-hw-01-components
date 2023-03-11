08fc8d4cd674eb68eb3eda9a7fcd08fe5f174be3
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-11 11:15:20 +0100

    Statistics component created
    with css module

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import css from './Statistics.module.css';
+
+const Statistics = ({title, stats}) => {
+return (<section className="statistics">
+    {title && <h2 className="title">{title}</h2>}
+
+<ul className="stat-list">
+{stats.map(({id, label, percentage}) => {
+    return (  <li className="item" key={id}>
+    <span className="label">{label}</span>
+    <span className="percentage">{percentage}</span>
+  </li>
+
+    );
+})}
+
+</ul>
+</section>);
+};
+
+Statistics.propTypes = {
+title: PropTypes.string.isRequired,
+stats: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
+}).isRequired).isRequired,
+};
+
+export default Statistics;
