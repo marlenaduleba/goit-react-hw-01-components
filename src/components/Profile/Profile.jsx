fa2c4df00ce83fbf7f4fe5f125b154adfc91bd73
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-11 11:16:07 +0100

    prop-types added
    and css module created

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,7 @@
-export const Profile = ({ username, tag, location, avatar, stats }) => {
+import PropTypes from "prop-types";
+import css from './Profile.module.css';
+
+ const Profile = ({ username, tag, location, avatar, stats: {followers, views, likes} }) => {
   return (<div className="profile">
   <div className="description">
     <img
@@ -7,23 +10,33 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
       className="avatar"
     />
     <p className="name">{username}</p>
-    <p className="tag">{tag}</p>
+    <p className="tag">@{tag}</p>
     <p className="location">{location}</p>
   </div>
 
   <ul className="stats">
     <li>
       <span className="label">Followers</span>
-      <span className="quantity">{stats.followers}</span>
+      <span className="quantity">{followers}</span>
     </li>
     <li>
       <span className="label">Views</span>
-      <span className="quantity">{stats.views}</span>
+      <span className="quantity">{views}</span>
     </li>
     <li>
       <span className="label">Likes</span>
-      <span className="quantity">{stats.likes}</span>
+      <span className="quantity">{likes}</span>
     </li>
   </ul>
 </div>);
 };
+
+Profile.propTypes = {
+username: PropTypes.string.isRequired,
+tag: PropTypes.string.isRequired,
+location: PropTypes.string.isRequired,
+avatar: PropTypes.string.isRequired, 
+stats: PropTypes.objectOf(PropTypes.number).isRequired,
+}; 
+
+export default Profile;
