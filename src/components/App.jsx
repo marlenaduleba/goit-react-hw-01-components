6a7c84ca5592f555cdf8c9c856b826e69b926664
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-11 11:17:02 +0100

    Statistics component added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
-import { Profile } from './Profile/Profile';
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+
 import user from '../data/user.json';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
-  
+  <>
   <Profile
   username={user.username}
   tag={user.tag}
@@ -11,6 +14,12 @@ export const App = () => {
   avatar={user.avatar}
   stats={user.stats}
 />
-  
+
+<Statistics
+title="Upload stats"
+stats={data}
+/>
+<Statistics stats={data} />
+  </>
   );
 };
