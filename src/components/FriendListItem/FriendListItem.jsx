6cbdf02f9cc8ca021960df116956008478ce328c
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-12 16:20:04 +0100

    FriendListItem/FriendList components styled

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,9 +2,10 @@
 import PropTypes from 'prop-types';
 import css from '../FriendListItem/FriendListItem.module.css';
 const FriendListItem = ({avatar, name, isOnline}) => {
+  const colorStatus = isOnline ? 'green' : 'red';
 return (
     <li className={css.item} >
-  <span className={css.status}></span>
+  <span className={`${css.status} ${css[colorStatus]}`}></span>
   <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
   <p className={css.name}>{name}</p>
 </li>
