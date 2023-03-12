c3284c3a631a0a18843d37220a0a695af81a107c
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-12 16:53:43 +0100

    corrected with Prettier

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,37 +1,36 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import css from '../TransactionHistory/TransactionHistory.module.css';
 
-const TransactionHistory = ({items}) => {
-return (
+const TransactionHistory = ({ items }) => {
+  return (
     <table className={css.transactionHistory}>
-  <thead className={css.thead}>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+      <thead className={css.thead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-  <tbody className={css.tbody}>
-{
-    items.map(({id, type, amount, currency}) => {
-        return (
-             <tr key={id} className={css.trow}>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
-    </tr>
-        )
-    })
-}
-  </tbody>
-</table>
-)
-
+      <tbody className={css.tbody}>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id} className={css.trow}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string.isRequired).isRequired).isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.objectOf(PropTypes.string.isRequired).isRequired
+  ).isRequired,
 };
 
 export default TransactionHistory;
